ae83914c871d669d9aa8a7fbcc32a6f0c32d1772
rin <sadmeowkins@outlook.com>
2022-05-09 21:49:33 +0800

    Get game by unique remote ID

--- Tracker.Core/Games/GetGame.cs
+++ Tracker.Core/Games/GetGame.cs
@@ -20,33 +20,39 @@ public class GetGameValidator : AbstractValidator<GetGameQuery>
 }
 
 public record GetGameResult(
-    long Id,
-    string? CoverImageURL,
-    string? Title,
-    string? Summary,
+    long RemoteId,
+    string CoverImageURL,
+    string Title,
+    string Summary,
     double Rating,
-    List<string> Platforms,
-    List<string> Companies
+    List<string>? Platforms,
+    List<string>? Companies
 );
 
 public static class GetGameMappings
 {
     public static void Map(Profile profile)
     {
+        profile.CreateMap<APIGame, Game>()
+            .ForMember(game => game.Id,
+                options => options.Ignore())
+            .ForMember(
+                game => game.RemoteId,
+                options => options.MapFrom(apiGame => apiGame.Id))
+            .ForMember(
+                game => game.PlatformsString,
+                options => options.MapFrom(apiGame => string.Join(";", apiGame.Platforms)))
+            .ForMember(
+                game => game.CompaniesString,
+                options => options.MapFrom(apiGame => string.Join(";", apiGame.Companies)));
+
         profile.CreateMap<Game, GetGameResult>()
-            .ForSourceMember(game => game.Id,
-                options => options.DoNotValidate())
-            .ForMember(
-                result => result.Id,
-                options => options.MapFrom(game => game.RemoteId))
-            .ForMember(
-                result => result.Platforms,
-                options => options.MapFrom(game => game.PlatformsString.Split(';', StringSplitOptions.None))
-            )
-            .ForMember(
-                result => result.Companies,
-                options => options.MapFrom(game => game.CompaniesString.Split(';', StringSplitOptions.None))
-            );
+            .ForCtorParam(
+                "Platforms",
+                options => options.MapFrom(game => game.PlatformsString.Split(';', StringSplitOptions.None)))
+            .ForCtorParam(
+                "Companies",
+                options => options.MapFrom(game => game.CompaniesString.Split(';', StringSplitOptions.None)));
     }
 }
 
@@ -62,27 +68,24 @@ public class GetGameHandler : IRequestHandler<GetGameQuery, GetGameResult>
         _gameService = gameService;
         _mapper = mapper;
     }
-    
+
     public async Task<GetGameResult> Handle(GetGameQuery getGameQuery, CancellationToken cancellationToken)
     {
         // Find game from database (cached locally).
-        Game? dbGame = await _dbContext.Games
+        var dbGame = await _dbContext.Games
             .AsNoTracking()
             .Where(game => game.RemoteId == getGameQuery.GameId)
             .FirstOrDefaultAsync(cancellationToken);
-        if (dbGame != null)
-        {
-            return _mapper.Map<Game, GetGameResult>(dbGame);
-        }
-        
+        if (dbGame != null) return _mapper.Map<Game, GetGameResult>(dbGame);
+
         // Find game from remote if not cached.
-        APIGame? remoteGame = await _gameService.GetGameById(getGameQuery.GameId);
+        var remoteGame = await _gameService.GetGameById(getGameQuery.GameId);
         if (remoteGame != null)
         {
-            Game newDBGame = _mapper.Map<APIGame, Game>(remoteGame);
+            var newDBGame = _mapper.Map<APIGame, Game>(remoteGame);
             _dbContext.Games.Add(newDBGame);
             await _dbContext.SaveChangesAsync(cancellationToken);
-          
+
             return _mapper.Map<Game, GetGameResult>(newDBGame);
         }
 
