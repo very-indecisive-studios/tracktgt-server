434112a2c511dae98bf4e4aa020448b2fffa2c3a
rin <sadmeowkins@outlook.com>
2022-05-13 21:50:03 +0800

    Expose use cases to endpoints

--- Tracker.API/Controllers/GameController.cs
+++ Tracker.API/Controllers/GameController.cs
@@ -1,8 +1,8 @@
 ﻿using System.Net.Mime;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Tracker.Core.Common;
 using Tracker.Core.Games;
-using Unit = MediatR.Unit;
 
 namespace Tracker.API.Controllers;
 
@@ -18,6 +18,55 @@ public class GameController : APIControllerBase
     public Task<Unit> AddTrackedGame(AddTrackedGameCommand addTrackedGameCommand)
     {
         return Mediator.Send(addTrackedGameCommand);
+    }
+    
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [Consumes(MediaTypeNames.Application.Json)]
+    [Produces(MediaTypeNames.Application.Json)]
+    [HttpDelete("track/remove", Name = nameof(RemoveTrackedGame))]
+    public Task<Unit> RemoveTrackedGame(RemoveTrackedGameCommand removeTrackedGameCommand)
+    {
+        return Mediator.Send(removeTrackedGameCommand);
+    }
+    
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [Consumes(MediaTypeNames.Application.Json)]
+    [Produces(MediaTypeNames.Application.Json)]
+    [HttpPut("track/update", Name = nameof(UpdateTrackedGame))]
+    public Task<Unit> UpdateTrackedGame(UpdateTrackedGameCommand updateTrackedGameCommand)
+    {
+        return Mediator.Send(updateTrackedGameCommand);
+    }
+    
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [Consumes(MediaTypeNames.Application.Json)]
+    [Produces(MediaTypeNames.Application.Json)]
+    [HttpGet("track/get", Name = nameof(GetTrackedGames))]
+    public Task<PagedListResult<GetTrackedGamesItemResult>> GetTrackedGames([FromQuery] 
+        GetTrackedGamesQuery getTrackedGamesQuery)
+    {
+        return Mediator.Send(getTrackedGamesQuery);
+    }
+    
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [Consumes(MediaTypeNames.Application.Json)]
+    [Produces(MediaTypeNames.Application.Json)]
+    [HttpGet("track/get/{userRemoteId}/{gameRemoteId:long}", Name = nameof(GetTrackedGame))]
+    public Task<GetTrackedGameResult> GetTrackedGame(string userRemoteId, long gameRemoteId)
+    {
+        return Mediator.Send(new GetTrackedGameQuery(userRemoteId, gameRemoteId));
     }
     
     [ProducesResponseType(StatusCodes.Status200OK)]
