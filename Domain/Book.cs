06733edc0beb6fe35a0ef39f52b51e3a2fe775f0
rin <sadmeowkins@outlook.com>
2022-05-30 16:24:39 +0800

    Entities for books and book trackings

---
+++ Domain/Book.cs
@@ -0,0 +1,16 @@
+﻿namespace Domain;
+
+#nullable disable
+
+public class Book : Entity
+{
+    public string RemoteId { get; set; }
+    
+    public string CoverImageURL { get; set; }
+    
+    public string Title { get; set; }
+    
+    public string Summary { get; set; }
+    
+    public string AuthorsString { get; set; }
+}
---
+++ Domain/BookTracking.cs
@@ -0,0 +1,24 @@
+﻿namespace Domain;
+
+#nullable disable
+
+public enum BookTrackingFormat { Digital, Physical, None }
+
+public enum BookTrackingStatus { Completed, Reading, Paused, Planning  }
+
+public enum BookTrackingOwnership { Owned, Loan, Wishlist }
+
+public class BookTracking : Entity
+{
+    public string UserRemoteId { get; set; }
+    
+    public string BookRemoteId { get; set; }
+    
+    public float ChaptersRead { get; set; }
+
+    public BookTrackingFormat Format { get; set; }
+
+    public BookTrackingStatus Status { get; set; }
+    
+    public BookTrackingOwnership Ownership { get; set; }
+}
