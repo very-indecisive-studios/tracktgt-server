614939098d2ea2c28a510496945a0d8e364a90eb
rin <sadmeowkins@outlook.com>
2022-05-31 14:59:41 +0800

    Book tracking use cases and tests

---
+++ Core.Test/Books/GetBookTrackingTest.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Core.Books;
+using Domain;
+using Persistence;
+
+namespace Core.Test.Books;
+
+[TestClass]
+public class GetBookTrackingTest
+{
+    private static SqliteConnection? Connection { get; set; }
+
+    private static DbContextOptions<DatabaseContext>? ContextOptions { get; set; }
+
+    private static DatabaseContext? InMemDatabase { get; set; }
+
+    private static IMapper? Mapper { get; set; }
+
+    private static GetBookTrackingHandler? GetBookTrackingHandler { get; set; }
+
+    private const string FakeUserRemoteId = "d33Z_NuT5";
+    private const string FakeBookRemoteId = "0";
+
+    [ClassInitialize]
+    public static async Task TestClassInit(TestContext context)
+    {
+        var fakeBookTrackingsList = new List<BookTracking>()
+        {
+            new()
+            {
+                UserRemoteId = FakeUserRemoteId,
+                BookRemoteId = FakeBookRemoteId,
+                ChaptersRead = 100,
+                Format = BookTrackingFormat.Digital,
+                Status = BookTrackingStatus.Reading,
+                Ownership = BookTrackingOwnership.Wishlist
+            }
+        };
+
+        // Setup in memory database
+        Connection = new SqliteConnection("Filename=:memory:");
+        Connection.Open();
+
+        ContextOptions = new DbContextOptionsBuilder<DatabaseContext>()
+            .UseSqlite(Connection)
+            .Options;
+
+        InMemDatabase = new DatabaseContext(ContextOptions);
+        await InMemDatabase.Database.EnsureCreatedAsync();
+        InMemDatabase.BookTrackings.AddRange(fakeBookTrackingsList);
+        await InMemDatabase.SaveChangesAsync();
+
+        var mappingConfig = new MapperConfiguration(mc => { mc.AddProfile<MappingProfiles>(); });
+        Mapper = mappingConfig.CreateMapper();
+
+        GetBookTrackingHandler = new GetBookTrackingHandler(InMemDatabase, Mapper);
+    }
+
+    [ClassCleanup]
+    public static async Task TestClassCleanup()
+    {
+        await Connection!.DisposeAsync();
+    }
+
+    [TestMethod]
+    public async Task GetBookTracking_Found()
+    {
+        // Setup
+        var query = new GetBookTrackingQuery(FakeUserRemoteId, FakeBookRemoteId);
+
+        // Execute
+        var result = await GetBookTrackingHandler!.Handle(query, CancellationToken.None);
+        
+        // Verify
+        Assert.IsNotNull(result);
+    }
+    
+    [TestMethod]
+    public async Task GetBookTracking_NotFound()
+    {
+        // Setup
+        var query = new GetBookTrackingQuery(FakeUserRemoteId, "BOOKNOTEXIST");
+
+        // Execute
+        var result = await GetBookTrackingHandler!.Handle(query, CancellationToken.None);
+        
+        // Verify
+        Assert.IsNull(result);
+    }
+}
---
+++ Core/Books/AddBookTracking.cs
@@ -0,0 +1,104 @@
+﻿using AutoMapper;
+using Core.Exceptions;
+using FluentValidation;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Domain;
+using Persistence;
+using Service.Book;
+
+namespace Core.Books;
+
+public record AddBookTrackingCommand(
+    string UserRemoteId,
+    string BookRemoteId,
+    int ChaptersRead,
+    BookTrackingFormat Format,
+    BookTrackingStatus Status,
+    BookTrackingOwnership Ownership
+) : IRequest<Unit>;
+
+public class AddBookTrackingValidator : AbstractValidator<AddBookTrackingCommand>
+{
+    public AddBookTrackingValidator()
+    {
+        RuleFor(c => c.UserRemoteId).NotEmpty();
+        RuleFor(c => c.BookRemoteId).NotEmpty();
+    }
+}
+
+public static class AddBookTrackingMappings
+{
+    public static void Map(Profile profile)
+    {
+        profile.CreateMap<AddBookTrackingCommand, BookTracking>();
+        
+        // APIBook => Book mapping exists in GetBook use case.
+    }
+}
+
+public class AddBookTrackingHandler : IRequestHandler<AddBookTrackingCommand, Unit>
+{
+    private readonly DatabaseContext _dbContext;
+    private readonly IBookService _bookService;
+    private readonly IMapper _mapper;
+
+    public AddBookTrackingHandler(DatabaseContext dbContext, IBookService bookService, IMapper mapper)
+    {
+        _dbContext = dbContext;
+        _bookService = bookService;
+        _mapper = mapper;
+    }
+
+    public async Task<Unit> Handle(AddBookTrackingCommand command, CancellationToken cancellationToken)
+    {
+        // Verify user.
+        bool isUserExists = await _dbContext.Users
+            .AsNoTracking()
+            .Where(u => u.RemoteId == command.UserRemoteId)
+            .AnyAsync(cancellationToken);
+
+        if (!isUserExists)
+        {
+            throw new NotFoundException("User not found!");
+        }
+        
+        // Verify if tracked book already exist.
+        bool isBookTrackingExists = await _dbContext.BookTrackings
+            .AsNoTracking()
+            .Where(bt => bt.BookRemoteId == command.BookRemoteId 
+                         && bt.UserRemoteId == command.UserRemoteId)
+            .AnyAsync(cancellationToken);
+
+        if (isBookTrackingExists)
+        {
+            throw new ExistsException("Tracked book already exists!");
+        }
+        
+        // Verify book id.
+        bool isBookExists = await _dbContext.Books
+            .AsNoTracking()
+            .Where(b => b.RemoteId == command.BookRemoteId)
+            .AnyAsync(cancellationToken);
+        // Fetch from external API and store in db if book not cached
+        if (!isBookExists)
+        {
+            APIBook? apiBook = await _bookService.GetBookById(command.BookRemoteId);
+
+            if (apiBook == null)
+            {
+                throw new NotFoundException("Book not found!");
+            }
+
+            Book book = _mapper.Map<APIBook, Book>(apiBook);
+            _dbContext.Books.Add(book);
+        }
+
+        var bookTracking = _mapper.Map<AddBookTrackingCommand, BookTracking>(command);
+        _dbContext.BookTrackings.Add(bookTracking);
+        
+        await _dbContext.SaveChangesAsync(cancellationToken);
+        
+        return Unit.Value;
+    }
+}
